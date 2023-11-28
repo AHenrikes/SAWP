011fce02bf0435067426a5b20762a61e389e16fe
Yousef Khalid <alshaikhliyousuf7777@gmail.com>
2023-11-28 15:59:08 +0300

    edit: changed key formats

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,25 +7,28 @@ export default {
   theme: {
     screens: {
       // Responsive most common screen sizes
-      'mobile': '375px',
-      'tablet': '1024',
-      'desktop': '1440px'
+      xsm: '375px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xlg: '1280px',
+      xxlg: '1440px'
     },
     // The project's color palette
     colors: {
       // Background colors
-      'mainColor': '#001120',
-      'mainSubColor': '#000c18',
-      'mainLinearStart': '#000c18',
-      'mainLinearEnd': '#001120',
+      mainColor: '#001120',
+      mainSubColor: '#000c18',
+      mainLinearStart: '#000c18',
+      mainLinearEnd: '#001120',
       // Text colors
-      'textColor1': '#ffffff',
-      'textColor2': '#808080',
+      textColor1: '#ffffff',
+      textColor2: '#808080',
       // Border colors
-      'borderColor1': '#003360',
+      borderColor1: '#003360',
       // Border gradient colors
-      'borderGradientStart': '#000585',
-      'borderGradientEnd': '#85006F'
+      borderGradientStart: '#000585',
+      borderGradientEnd: '#85006F'
     },
     extend: {},
   },
