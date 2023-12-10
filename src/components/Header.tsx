641d105a63fc978bb1c092580bfc0d6b342aedb1
Yousef Khalid <alshaikhliyousuf7777@gmail.com>
2023-12-11 01:32:11 +0300

    edit: changed live clock js to react format

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Header() {
-  function displayTime() {
-    let currentDate = new Date();
-    let hours = currentDate.getHours();
-    let minutes = currentDate.getMinutes();
-    let seconds = currentDate.getSeconds();
-    let timeElem = document.getElementById("time");
-    let timeString = `00:${hours}:${minutes}:${seconds}`;
-    timeElem.innerText = timeString
-      .split(":")
-      .map((e) => `0${e}`.slice(-2))
-      .join(":");
-  }
-  setInterval(displayTime, 1000);
+  const [dateTime, setDateTime] = React.useState("");
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      const currentDate = new Date();
+      const hours = String(currentDate.getHours()).padStart(2, "0");
+      const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+      const seconds = String(currentDate.getSeconds()).padStart(2, "2");
+
+      const updatedDateTime = `00:${hours}:${minutes}:${seconds}`;
+      setDateTime(updatedDateTime);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
   return (
     <div>
       <nav>
         <ul className="grid p-4" id="headerList">
           <li className="logo">
-            <a href="#" className="font-krona text-textColor1 drop-shadow-mainShadow text-[20px]">
+            <a href="#" className="font-krona text-textColor1 drop-shadow-mainShadow text-[20px] md:text-[24px] xxlg:text-[32px]">
               SAWP
             </a>
           </li>
           <li className="time">
-            <p className="font-orbitron text-textColor2 opacity-40 text-[20px]" id="time">
-              00:15:36:69
+            <p className="font-orbitron text-textColor2 opacity-40 text-[20px] md:text-[24px] xxlg:text-[32px]" id="time">
+              {dateTime}
             </p>
           </li>
           <li className="github-icon ml-auto hover:brightness-150">
