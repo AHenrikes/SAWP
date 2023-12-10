f26a40902282fe7b00b23de9391afbe26d1cba2d
Yousef Khalid <alshaikhliyousuf7777@gmail.com>
2023-12-10 05:58:41 +0300

    added: component styling && timer functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,35 @@
 import React from "react";
 
 function Header() {
+  function displayTime() {
+    let currentDate = new Date();
+    let hours = currentDate.getHours();
+    let minutes = currentDate.getMinutes();
+    let seconds = currentDate.getSeconds();
+    let timeElem = document.getElementById("time");
+    let timeString = `00:${hours}:${minutes}:${seconds}`;
+    timeElem.innerText = timeString
+      .split(":")
+      .map((e) => `0${e}`.slice(-2))
+      .join(":");
+  }
+  setInterval(displayTime, 1000);
   return (
-    <div className="grid">
+    <div>
       <nav>
-        <ul>
-          <li>
-            <a href="#">SAWP</a>
+        <ul className="grid p-4" id="headerList">
+          <li className="logo">
+            <a href="#" className="font-krona text-textColor1 drop-shadow-mainShadow text-[20px]">
+              SAWP
+            </a>
           </li>
-          <li>
-            <p>00:15:36:69</p>
+          <li className="time">
+            <p className="font-orbitron text-textColor2 opacity-30 text-[20px]" id="time">
+              00:15:36:69
+            </p>
           </li>
-          <li>
-            <img src="./src/assets/images/github.png" alt="github icon"/>
+          <li className="github-icon ml-auto">
+            <img className="" src="./src/assets/images/github.png" alt="github icon" />
           </li>
         </ul>
       </nav>
