c98fdf5a3d4dad2d3ef20bfc154a9fb44919af94
YousefKhalid-iq <alshaikhliyousuf7777@gmail.com>
2023-12-26 20:18:30 +0300

    edit: added main swap functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,39 @@ import UserInput from "./components/UserInput";
 import Footer from "./components/Footer";
 
 function App() {
+
+  const [text, setText] = useState('');
+
+  // get a random number
+
+  const getRandomInt = (n: number) => {
+    return Math.floor(Math.random() * n);
+  }
+
+  // shuffle the string in the output window
+
+  const shuffle = (s: string) => {
+    let arr = s.split('');
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      let j = getRandomInt(arr.length);
+
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+
+    s = arr.join('');
+    return s
+  }
+  let s = text
+  s = shuffle(s);
+
   return (
-    <div className="bg-gradient-to-b from-mainSubColor to-mainColor h-screen grid" id="mainCont">
+    <div className="bg-gradient-to-b from-mainSubColor to-mainColor h-screen grid overflow-hidden" id="mainCont">
       <Header />
-      <OutputWindow />
-      <UserInput />
+      <OutputWindow s={s}/>
+      <UserInput setText={setText}/>
       <Footer />
     </div>
   );
--- src/components/OutputWindow.tsx
+++ src/components/OutputWindow.tsx
@@ -1,16 +1,13 @@
-import React from "react";
+// component used for displaying the input text after being swapped
 
-function OutputWindow() {
+function OutputWindow(props: {s: string}){
+  const {s} = props
   return (
     <div className="flex flex-col w-[80%] md:w-[70%] xxlg:w-[60%] m-auto py-6 relative" id="output">
-      <button className="absolute right-[3px] top-[3px]">
-        <img className="" src="./src/assets/images/Vector.png" alt="copy to clipboard icon" />
+      <button className="absolute right-[3px] top-[3px] hover:brightness-150">
+        <img className="text-textColor1" src="./src/assets/vector.png" alt="copy to clipboard icon" />
       </button>
-      <textarea
-        // readOnly
-        placeholder="tpye hree..."
-        className="bg-outputFill h-40 sm:h-56 md:h-72 lg:h-80 xxlg:h-96 placeholder:font-roboto border-y-[1px] input_output_style resize-none"
-      ></textarea>
+      <p data-placeholder="tpye hree..." className="bg-outputFill h-40 sm:h-56 md:h-72 lg:h-80 xxlg:h-96 placeholder:font-roboto border-y-[1px] input_output_style overflow-hidden">{s}</p>
     </div>
   );
 }
